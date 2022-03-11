bb2ddc0663225ca576c2b0278a90fe8d56f4eb6d
cas1908 <schima05@gmail.com>
2022-03-11 04:54:02 +0100

    Using the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Program that prints "programming is like a building a multilingual puzzle
+ * Outputs (0)
+ */
+ int main()
+ {
+	puts("Programming is like building a multilingual puzzle"\n");
+	return (0)
+ }
