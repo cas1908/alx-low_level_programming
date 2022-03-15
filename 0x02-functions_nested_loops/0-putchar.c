b802c32d8df2d1c5db4a5a208c5a30432aed135f
cas1908 <schima05@gmail.com>
2022-03-15 19:59:19 +0100

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ * mainm To print _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		-putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
