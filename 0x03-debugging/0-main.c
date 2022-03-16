37faee144328564ddcdb249cc6b22285eaef4e27
cas1908 <schima05@gmail.com>
2022-03-16 07:12:18 +0100

    Files created both Header and Code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Testing if a number is positive or negative
+ *
+ * Return: Always 0 (succes#include "main.h"
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+		return (0);
+	}
+	else (n < 0)
+	{
+		printf("%d is negative\n", i);
+		return (0);
+	}
+
+}
